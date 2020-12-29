17cf83c02b08be97e5a7db4e5993e2782050ced4
Bryan Burke <bryan.t.burke@gmail.com>
2020-12-29 12:30:52 +0200

    adds session delete test

--- store_test.go
+++ store_test.go
@@ -94,3 +94,32 @@ func TestSessionExpires(t *testing.T) {
 	assert.NoError(t, err)
 	assert.True(t, sess2.IsNew)
 }
+
+func TestSessionDelete(t *testing.T) {
+	tmpdir, err := ioutil.TempDir("", "store-test")
+	require.NoError(t, err)
+	path := filepath.Join(tmpdir, "test.db")
+	db, err := sql.Open("sqlite3", path)
+	require.NoError(t, err)
+	defer os.RemoveAll(tmpdir)
+
+	store, err := NewStore(db, securecookie.GenerateRandomKey(32))
+	require.NoError(t, err)
+
+	r := httptest.NewRequest("GET", "/", nil)
+	sess, err := store.New(r, "test")
+	assert.NoError(t, err)
+	assert.True(t, sess.IsNew)
+
+	sess.Options = &sessions.Options{MaxAge: -1}
+	w := httptest.NewRecorder()
+	assert.NoError(t, sess.Save(r, w))
+
+	r2 := httptest.NewRequest("GET", "/", nil)
+	r2.Header.Add("Cookie", w.Header().Get("Set-Cookie"))
+
+	// session should immediately expire
+	sess2, err := store.New(r2, "test")
+	assert.NoError(t, err)
+	assert.True(t, sess2.IsNew)
+}
